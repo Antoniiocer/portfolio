bb59fcf14837ae7fad627aec4fc1fb3ead2a26ba
Antoniiocer <antoniocervantes.3005@gmail.com>
2025-02-27 18:17:18 +0100

    fix(main.tsx) fixed main.tsx

--- main.tsx
+++ main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import App from './src/App.tsx'
+import App from './src/App'
 import './src/styles/index.scss';
 
 createRoot(document.getElementById('root')!).render(
